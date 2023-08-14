bf754eba18118ab24aef826315ee0d3c762deb51
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-13 22:19:10 -0300

    :sparkles: feat: adicionado componentes no module

--- lavanderia-front/src/app/app.module.ts
+++ lavanderia-front/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { OrderComponent } from './pages/order/order.component';
+import { ButtonsComponent } from './components/ui-components/buttons/buttons.component';
+import { InputsComponent } from './components/ui-components/inputs/inputs.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    OrderComponent,
+    ButtonsComponent,
+    InputsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
