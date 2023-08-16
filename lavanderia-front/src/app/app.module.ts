8a2c71bed7ec87bec80be8baa78058727f2700d7
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-15 23:13:43 -0300

    :sparkles: feat: adicionado componentes no module

--- lavanderia-front/src/app/app.module.ts
+++ lavanderia-front/src/app/app.module.ts
@@ -12,6 +12,11 @@ import { InputsComponent } from './components/ui-components/inputs/inputs.compon
 
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ItemsComponent } from './components/ui-components/items/items.component';
+import { BoxComponent } from './components/ui-components/box/box.component';
+import { NavBarComponent } from './components/ui-components/nav-bar/nav-bar.component';
+import { SideBarComponent } from './components/ui-components/side-bar/side-bar.component';
+import { MaskComponent } from './components/ui-components/mask/mask.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +27,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     OrderComponent,
     ButtonsComponent,
     InputsComponent,
+    ItemsComponent,
+    BoxComponent,
+    NavBarComponent,
+    SideBarComponent,
+    MaskComponent,
   ],
   imports: [
     BrowserModule,
