a4ae063e7472e8746b02a965a5ebfc0d128b2edd
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-15 23:11:17 -0300

    :sparkles: feat: novos caminhos no roteamento e layout da máscara

--- lavanderia-front/src/app/app-routing.module.ts
+++ lavanderia-front/src/app/app-routing.module.ts
@@ -2,11 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { OrderComponent } from './pages/order/order.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MaskComponent } from './components/ui-components/mask/mask.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: MaskComponent,
+    children: [
+      { path: 'order', component: OrderComponent },
+      { path: 'home', component: HomeComponent },
+    ],
+  },
 ];
 
 @NgModule({
