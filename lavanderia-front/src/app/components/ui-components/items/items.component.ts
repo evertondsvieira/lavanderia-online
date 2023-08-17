7d37ed2c6cf1af7fbe688b2936627a66db5fbaf4
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-17 00:12:44 -0300

    :sparkles: feat: alterado nomes dos titulos

--- lavanderia-front/src/app/components/ui-components/items/items.component.ts
+++ lavanderia-front/src/app/components/ui-components/items/items.component.ts
@@ -1,43 +1,41 @@
 import { Component } from '@angular/core';
 
 interface IItem {
-  id: number
-  imageSrc: string
-  title: string
-  description: string
+  id: number;
+  imageSrc: string;
+  title: string;
+  description: string;
 }
 
 @Component({
   selector: 'app-items',
   templateUrl: './items.component.html',
 })
-
 export class ItemsComponent {
   items: IItem[] = [
     {
       id: 1,
       imageSrc: 'assets/lavagem.png',
-      title: 'Item 1',
-      description: 'Descrição do Item 1'
+      title: 'Lavagem e Secagem regular',
+      description: 'Descrição do Item 1',
     },
     {
       id: 2,
       imageSrc: 'assets/lavagem.png',
-      title: 'Item 2',
-      description: 'Descrição do Item 2'
+      title: 'Lavagem a seco',
+      description: 'Descrição do Item 2',
     },
     {
       id: 3,
       imageSrc: 'assets/lavagem.png',
-      title: 'Item 3',
-      description: 'Descrição do Item 2'
+      title: 'Passar roupa',
+      description: 'Descrição do Item 3',
     },
     {
       id: 4,
       imageSrc: 'assets/lavagem.png',
-      title: 'Item 4',
-      description: 'Descrição do Item 2'
+      title: 'Lavagem tênis e calçados',
+      description: 'Descrição do Item 4',
     },
   ];
 }
-
