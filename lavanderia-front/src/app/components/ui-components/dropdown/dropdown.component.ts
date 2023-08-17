0cc528a05a534dbbaa56050c3ecd5f9a84522fcf
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-17 00:10:49 -0300

    :sparkles: feat: criado componente de dropdown

---
+++ lavanderia-front/src/app/components/ui-components/dropdown/dropdown.component.ts
@@ -0,0 +1,38 @@
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { createPopper } from '@popperjs/core';
+
+@Component({
+  selector: 'app-dropdown',
+  templateUrl: './dropdown.component.html',
+})
+export class DropdownComponent implements AfterViewInit {
+  dropdownPopoverShow = false;
+  selectedFilter: string = ''; // Campo para armazenar o filtro selecionado
+  @ViewChild('btnDropdownRef', { static: false }) btnDropdownRef!: ElementRef;
+  @ViewChild('popoverDropdownRef', { static: false })
+  popoverDropdownRef!: ElementRef;
+
+  dropdown: string[] = ['Aberto', 'Fechado', 'Em entrega'];
+
+  constructor() {}
+
+  ngAfterViewInit() {
+    createPopper(
+      this.btnDropdownRef.nativeElement,
+      this.popoverDropdownRef.nativeElement,
+      {
+        placement: 'bottom-start',
+      },
+    );
+  }
+
+  toggleDropdown(event: Event) {
+    event.preventDefault();
+    this.dropdownPopoverShow = !this.dropdownPopoverShow;
+  }
+
+  applyFilter(filter: string) {
+    this.selectedFilter = filter;
+    this.dropdownPopoverShow = false; // Fechar o dropdown após selecionar um filtro
+  }
+}
