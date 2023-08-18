854670c8c31f8afa00941516d48d7a8b2f7ecd2f
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-17 23:43:55 -0300

    :recycle: refactor: refatorada navbar

--- lavanderia-front/src/app/components/ui-components/nav-bar/nav-bar.component.ts
+++ lavanderia-front/src/app/components/ui-components/nav-bar/nav-bar.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
 })
 export class NavBarComponent {
+  @ViewChild('drawerNavigation')
+  drawerNavigation!: ElementRef;
 
+  toggleDrawer(): void {
+    const drawer = this.drawerNavigation.nativeElement;
+    drawer.classList.toggle('translate-x-0');
+    drawer.classList.toggle('-translate-x-full');
+  }
+
+  closeDrawer(): void {
+    const drawer = this.drawerNavigation.nativeElement;
+    drawer.classList.remove('translate-x-0');
+    drawer.classList.add('-translate-x-full');
+  }
 }
