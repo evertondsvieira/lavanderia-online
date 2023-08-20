fa6fe84f83aaa1de6e9f3ec1640795a29f95dcef
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-20 01:53:34 -0300

    :lipstick: fix: alterado cores do projeto

--- lavanderia-front/tailwind.config.js
+++ lavanderia-front/tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        customRed: '#DA4B30',
-        customOrange: '#EE6F57',
+        mainBlue: '#0074B2',
+        secondaryBlue: '#66A3D2',
         customGray: '#f9fafc',
       },
     },
