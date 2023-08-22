07dc777090380bf95b63a88fbc1d40affef93f2e
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-21 21:58:52 -0300

    :bug: fix: corrigido theme e language popover

--- lavanderia-front/src/app/components/ui-components/language-popover/language-popover.component.ts
+++ lavanderia-front/src/app/components/ui-components/language-popover/language-popover.component.ts
@@ -5,27 +5,4 @@ import { createPopper } from '@popperjs/core';
   selector: 'app-language-popover',
   templateUrl: './language-popover.component.html',
 })
-export class LanguagePopoverComponent implements AfterViewInit {
-  dropdownPopoverShow = false;
-  @ViewChild('btnDropdownRef', { static: false })
-  btnDropdownRef!: ElementRef;
-  @ViewChild('popoverDropdownRef', { static: false })
-  popoverDropdownRef!: ElementRef;
-  ngAfterViewInit() {
-    createPopper(
-      this.btnDropdownRef.nativeElement,
-      this.popoverDropdownRef.nativeElement,
-      {
-        placement: 'right',
-      },
-    );
-  }
-  toggleDropdown(event: { preventDefault: () => void; }) {
-    event.preventDefault();
-    if (this.dropdownPopoverShow) {
-      this.dropdownPopoverShow = false;
-    } else {
-      this.dropdownPopoverShow = true;
-    }
-  }
-}
+export class LanguagePopoverComponent {}
--- lavanderia-front/src/app/components/ui-components/theme-popover/theme-popover.component.ts
+++ lavanderia-front/src/app/components/ui-components/theme-popover/theme-popover.component.ts
@@ -5,31 +5,4 @@ import { createPopper } from '@popperjs/core';
   selector: 'app-theme-popover',
   templateUrl: './theme-popover.component.html',
 })
-export class ThemePopoverComponent implements AfterViewInit {
-  dropdownPopoverShow = false;
-  @ViewChild('btnDropdownRef', { static: false })
-  btnDropdownRef!: ElementRef;
-  @ViewChild('popoverDropdownRef', { static: false })
-  popoverDropdownRef!: ElementRef;
-  ngAfterViewInit() {
-    createPopper(
-      this.btnDropdownRef.nativeElement,
-      this.popoverDropdownRef.nativeElement,
-      {
-        placement: 'right',
-      },
-    );
-  }
-  toggleDropdown(event: { preventDefault: () => void }) {
-    event.preventDefault();
-    if (this.dropdownPopoverShow) {
-      this.dropdownPopoverShow = false;
-    } else {
-      this.dropdownPopoverShow = true;
-    }
-  }
-
-  togglePopoverTheme() {
-    this.dropdownPopoverShow = !this.dropdownPopoverShow;
-  }
-}
+export class ThemePopoverComponent {}
