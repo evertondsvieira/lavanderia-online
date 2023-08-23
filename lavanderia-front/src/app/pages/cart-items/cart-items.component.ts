42b186471254b0f3b7ac6248b8de36afb0fbb975
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-22 23:52:48 -0300

    :sparkles: feat: criado página de carrinho

--- lavanderia-front/src/app/pages/cart-items/cart-items.component.ts
+++ lavanderia-front/src/app/pages/cart-items/cart-items.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DetalhesPedidoService } from '../detalhes-pedido/detalhes-pedido.service';
 
 @Component({
   selector: 'app-cart-items',
   templateUrl: './cart-items.component.html',
 })
-export class CartItemsComponent {
+export class CartItemsComponent implements OnInit {
+  itemId: number | null = null;
+  detalhes: string | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private detalhesPedidoService: DetalhesPedidoService,
+  ) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.itemId = id;
+      this.detalhes = this.detalhesPedidoService.getDetalhesById(id);
+    });
+  }
 }
