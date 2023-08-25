fd3d3c884eb25d082908aa3424f13127227c6199
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-08-25 00:26:41 -0300

    :sparkles: feat: adicionando select de status

--- lavanderia-front/src/app/pages/order/order.component.ts
+++ lavanderia-front/src/app/pages/order/order.component.ts
@@ -1,9 +1,58 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+interface IStatusOrder {
+  id: number;
+  title: string;
+  description: string;
+  statusBtn: string;
+}
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
 })
 export class OrderComponent {
+  selectedStatus: string = 'all';
+  statusOptions: string[] = ['Aberto', 'Cancelado', 'Rejeitado'];
 
+  pedidos: IStatusOrder[] = [
+    {
+      id: 3,
+      title: 'Pedido 3',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
+      statusBtn: 'Rejeitado',
+    },
+    {
+      id: 2,
+      title: 'Pedido 2',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
+      statusBtn: 'Aberto',
+    },
+    {
+      id: 1,
+      title: 'Pedido 1',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
+      statusBtn: 'Cancelado',
+    },
+  ];
+
+  get filteredPedidos() {
+    if (this.selectedStatus === 'all') {
+      return this.pedidos;
+    } else {
+      return this.pedidos.filter(
+        (pedido) => pedido.statusBtn === this.selectedStatus,
+      );
+    }
+  }
+
+  constructor(private router: Router) {}
+
+  showDetailsOrder(id: number) {
+    this.router.navigate(['/order', id]);
+  }
 }
