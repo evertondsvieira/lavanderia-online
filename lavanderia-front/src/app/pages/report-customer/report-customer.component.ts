b28ccfdc4ae45b6e35b760768995a7a3ce9fb9bd
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-09-23 20:26:56 -0300

    :sparkles: feat: criado página de relatório de clientes

---
+++ lavanderia-front/src/app/pages/report-customer/report-customer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-report-customer',
+  templateUrl: './report-customer.component.html',
+})
+export class ReportCustomerComponent {
+
+}
