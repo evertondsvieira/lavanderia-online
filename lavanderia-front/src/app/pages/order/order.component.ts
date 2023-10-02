7f9c40727434003628bdabe001830d5f03e1772b
Eduardo Costa Matos <eduardo.matos@ufpr.br>
2023-10-02 15:37:44 -0300

    :lipstick: fix: adicionado hora na tela /order

--- lavanderia-front/src/app/pages/order/order.component.ts
+++ lavanderia-front/src/app/pages/order/order.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
-interface IStatusOrder {
+export interface IStatusOrder {
   id: number;
   title: string;
   description: string;
   statusBtn: string;
+  status:string;
   date: Date;
 }
 
@@ -20,27 +21,17 @@ export class OrderComponent {
   pedidos: IStatusOrder[] = [
     {
       id: 3,
-      title: 'Pedido 3',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
-       date: new Date('2023-10-01 15:43'),
-        statusBtn: 'Rejeitado',        
+      title: 'Pedido 3', date: new Date('2023-10-01 15:43'), statusBtn: 'Rejeitado', status: 'Rejeitado',           
+       description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
     },
     {
-      id: 2,
-      title: 'Pedido 2',
-      description:
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
-        date: new Date('2023-09-30 09:56'),
-        statusBtn: 'Aberto',    
+      id: 2, title: 'Pedido 2', date: new Date('2023-09-30 09:56'), statusBtn: 'Aberto', status: 'Aberto',
+       description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',     
     },
     {
-      id: 1,
-      title: 'Pedido 1',
-      description:
+      id: 1, title: 'Pedido 1', date: new Date('2023-09-29 11:27'), statusBtn: 'Cancelado', status: 'Cancelado',    
+       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, nam tempore odio consequuntur optio cumque eum reprehenderit, hic alias autem temporibus veniam facere labore qui, magni suscipit ab repudiandae voluptate',
-        date: new Date('2023-09-29 11:27'),
-        statusBtn: 'Cancelado',       
     },
   ];
 
@@ -53,6 +44,26 @@ export class OrderComponent {
       );
     }
   }
+
+  selectedOrderToChangeStatus: IStatusOrder | null = null;
+
+  openConfirmationModal(pedido: IStatusOrder) {
+    this.selectedOrderToChangeStatus = pedido;
+  }
+
+  confirmStatusChange(pedido: IStatusOrder) {
+    if (pedido) {
+      if (pedido.status === 'Aberto'){
+        pedido.status = 'Pago';
+      this.selectedOrderToChangeStatus = null;
+    }
+  }
+}
+
+  cancelStatusChange() {
+    this.selectedOrderToChangeStatus = null;
+  }
+
   constructor(private router: Router) {}
 
   showDetailsOrder(id: number) {
