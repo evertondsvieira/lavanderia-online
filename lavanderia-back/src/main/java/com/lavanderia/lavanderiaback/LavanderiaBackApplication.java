f728f9ff7e1397d1d8318118bd9b5906ecad8ed7
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-10-27 19:05:45 -0300

    :wrench: chore: maven configurações

---
+++ lavanderia-back/src/main/java/com/lavanderia/lavanderiaback/LavanderiaBackApplication.java
@@ -0,0 +1,13 @@
+package com.lavanderia.lavanderiaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavanderiaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavanderiaBackApplication.class, args);
+	}
+
+}
---
+++ lavanderia-back/src/test/java/com/lavanderia/lavanderiaback/LavanderiaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanderia.lavanderiaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavanderiaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
