6f6fc89623efd9af23c28d916abf7c42231d5020
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-08 00:09:32 -0300

    :sparkles: feat: consumindo post, get, update e delete do crud da api

--- lavanderia-front/src/app/pages/employee-crud/employee-crud.component.ts
+++ lavanderia-front/src/app/pages/employee-crud/employee-crud.component.ts
@@ -1,26 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/app/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 export interface ClothingItem {
-  name: string;
-  imageUrl?: string;
-  price: string;
-  deliveryTime: string;
+  id: number;
+  nome: string;
+  valor: string;
+  imgUrl: string;
+  prazo: string;
+  descricao: string;
 }
 
 @Component({
   selector: 'app-employee-crud',
   templateUrl: './employee-crud.component.html',
 })
-export class EmployeeCrudComponent {
+export class EmployeeCrudComponent implements OnInit {
+  apiUrl = environment.apiUrl;
   listaVazia: boolean = false;
   selectedItem: ClothingItem | null = null;
   selectedIndex: number | null = null;
+  clothingItems: ClothingItem[] = [];
+
+  constructor(private http: HttpClient) {}
 
   novoItem: ClothingItem = {
-    name: '',
-    imageUrl: '',
-    price: '',
-    deliveryTime: '',
+    id: 0,
+    nome: '',
+    imgUrl: '',
+    valor: '',
+    prazo: '',
+    descricao: '',
   };
 
   itemEmEdicao: ClothingItem | null = null;
@@ -28,76 +38,118 @@ export class EmployeeCrudComponent {
   editItem(item: ClothingItem) {
     this.itemEmEdicao = { ...item };
     this.novoItem = { ...item };
-  }
 
-  updateItem() {
-    if (
-      this.itemEmEdicao &&
-      this.itemEmEdicao.name &&
-      this.itemEmEdicao.price &&
-      this.itemEmEdicao.deliveryTime
-    ) {
-      const index = this.clothingItems.findIndex(
-        (item) => item === this.itemEmEdicao,
-      );
-      if (index !== -1) {
-        this.clothingItems[index] = { ...this.itemEmEdicao };
-        this.itemEmEdicao = null;
-      }
-    } else if (
-      this.novoItem.name &&
-      this.novoItem.price &&
-      this.novoItem.deliveryTime
-    ) {
-      this.clothingItems.push({ ...this.novoItem });
-      this.novoItem = {
-        name: '',
-        imageUrl: '',
-        price: '',
-        deliveryTime: '',
+    if (!this.itemEmEdicao) {
+      this.itemEmEdicao = {
+        id: 0,
+        nome: '',
+        imgUrl: '',
+        valor: '',
+        prazo: '',
+        descricao: '',
       };
     }
   }
 
+  ngOnInit(): void {
+    this.http.get<ClothingItem[]>(this.apiUrl + 'item').subscribe({
+      next: (data: ClothingItem[]) => {
+        this.clothingItems = data;
+        console.log(this.clothingItems);
+      },
+      error: (error: any) => {
+        console.error('Erro ao buscar os dados:', error);
+      },
+    });
+  }
+
+  addItem() {
+    if (this.novoItem.nome && this.novoItem.valor && this.novoItem.prazo) {
+      const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  
+      this.http
+        .post<ClothingItem>(this.apiUrl + 'item', this.novoItem, { headers })
+        .subscribe({
+          next: (data: ClothingItem) => {
+            this.clothingItems.push(data);
+            this.novoItem = {
+              id: 0,
+              nome: '',
+              imgUrl: '',
+              valor: '',
+              prazo: '',
+              descricao: '',
+            };
+            this.listaVazia = false;
+          },
+          error: (error: any) => {
+            console.error('Erro ao adicionar o item:', error);
+          },
+        });
+    }
+  }
+ 
+
+  putItem() {
+    if (this.itemEmEdicao) {
+      this.itemEmEdicao = { ...this.itemEmEdicao, ...this.novoItem };
+  
+      const index = this.clothingItems.findIndex(
+        (item) => item.id === this.itemEmEdicao?.id
+      );
+      if (index !== -1) {
+        this.http
+          .put<ClothingItem>(
+            `${this.apiUrl}item/${this.itemEmEdicao.id}`,
+            this.itemEmEdicao
+          )
+          .subscribe({
+            next: (data: ClothingItem) => {
+              this.clothingItems[index] = data;
+              this.itemEmEdicao = null;
+            },
+            error: (error: any) => {
+              console.error('Erro ao atualizar o item:', error);
+            },
+          });
+      }
+    }
+  }
+  
+  updateItem() {
+    if (this.novoItem.id === 0) {
+      this.addItem()
+    } else {
+      this.putItem()
+    }   
+  }
+  
   removeItem(item: ClothingItem) {
     const index = this.clothingItems.indexOf(item);
     if (index !== -1) {
-      this.clothingItems.splice(index, 1);
+      const itemId = item.id;
+
+      this.http.delete(`${this.apiUrl}item/${itemId}`).subscribe({
+        next: () => {
+          this.clothingItems.splice(index, 1);
+          this.listaVazia = this.clothingItems.length === 0;
+        },
+        error: (error: any) => {
+          console.error('Erro ao excluir o item:', error);
+        },
+      });
     }
-    this.listaVazia = this.clothingItems.length === 0;
   }
 
   cleanForm() {
     this.itemEmEdicao = null;
     this.novoItem = {
-      name: '',
-      imageUrl: '',
-      price: '',
-      deliveryTime: '',
+      id: 0,
+      nome: '',
+      imgUrl: '',
+      valor: '',
+      prazo: '',
+      descricao: '',
     };
   }
-
-  clothingItems: ClothingItem[] = [
-    {
-      name: 'Camiseta',
-      imageUrl:
-        'https://static.netshoes.com.br/produtos/camiseta-ssb-brand-masculina-lisa-basica-100-algodao/16/70P-0003-016/70P-0003-016_zoom1.jpg?ts=1636498346&ims=544x',
-      price: 'R$ 5.00',
-      deliveryTime: '4 horas',
-    },
-    {
-      name: 'Camiseta',
-      imageUrl:
-        'https://static.netshoes.com.br/produtos/camiseta-ssb-brand-masculina-lisa-basica-100-algodao/16/70P-0003-016/70P-0003-016_zoom1.jpg?ts=1636498346&ims=544x',
-      price: 'R$ 5.00',
-      deliveryTime: '4 horas',
-    },
-    {
-      name: 'Camiseta',
-      imageUrl:
-        'https://static.netshoes.com.br/produtos/camiseta-ssb-brand-masculina-lisa-basica-100-algodao/16/70P-0003-016/70P-0003-016_zoom1.jpg?ts=1636498346&ims=544x',
-      price: 'R$ 5.00',
-      deliveryTime: '4 horas',
-    },
-  ];
 }
