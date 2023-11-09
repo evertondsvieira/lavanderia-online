7832f4d0e1d46cc2fe19c0ab2c9d36896943881e
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-09 00:00:46 -0300

    :bug: fix: provider do formulário

--- lavanderia-front/src/app/components/ui-components/inputs/inputs.component.ts
+++ lavanderia-front/src/app/components/ui-components/inputs/inputs.component.ts
@@ -1,12 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './inputs.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputsComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputsComponent {
+
+export class InputsComponent implements ControlValueAccessor {
   @Input() icon: string | null = null;
   @Input() size: 'xs' | 'sm' | 'lg' | 'default' = 'default';
   @Input() type: string = 'text';
   @Input() placeholder: string = '';
+  fieldValue: string = '';
+
+  constructor() {}
+
+  writeValue(value: any) {
+    this.fieldValue = value || '';
+  }
+
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any) {
+    this.onTouched = fn;
+  }
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  updateValue(value: string) {
+    this.fieldValue = value;
+    this.onChange(value);
+  }
+
+  onBlur() {
+    this.onTouched();
+  }
 }
