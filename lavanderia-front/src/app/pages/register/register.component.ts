34873707fa9ef53dd7651ce3d74df54a380f4705
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-09 00:01:16 -0300

    :sparkles: feat: consumindo da api para fazer o post do registro

--- lavanderia-front/src/app/pages/register/register.component.ts
+++ lavanderia-front/src/app/pages/register/register.component.ts
@@ -1,9 +1,99 @@
 import { Component } from '@angular/core';
+import { IProduct } from 'src/app/components/ui-components/product/product.component';
+import { environment } from 'src/app/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router'; 
+
+export interface IUser {
+  id: number;
+  nome: string;
+  telefone: string;
+  email: string;
+  cpf: string;
+  cep: string;
+  uf: string;
+  cidade: string;
+  bairro: string;
+  rua: string;
+  senha: string;
+  salt: string;
+  pedidos?: IProduct[];
+}
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
+  apiUrl = environment.apiUrl;
+  users: IUser[] = []
+  newUser: IUser = {
+    id: 0,
+    nome: '',
+    telefone: '',
+    email: '',
+    cpf: '',
+    cep: '',
+    uf: '',
+    cidade: '',
+    bairro: '',
+    rua: '',
+    senha: '',
+    salt: '',
+    pedidos: [],
+  }
 
+  constructor(private http: HttpClient, private router: Router) {} 
+
+  onSubmit() {
+    if (this.areAllFieldsFilled()) {
+      const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+      });
+
+      this.http
+        .post<IUser>(`${this.apiUrl}user`, this.newUser, { headers })
+        .subscribe({
+          next: (user) => {
+            this.users.push(user);
+  
+            this.newUser = {
+              id: 0,
+              nome: '',
+              telefone: '',
+              email: '',
+              cpf: '',
+              cep: '',
+              uf: '',
+              cidade: '',
+              bairro: '',
+              rua: '',
+              senha: '',
+              salt: '',
+              pedidos: [],
+            };
+            this.router.navigate(['/'])
+          },
+          error: (error) => {
+            console.error('Erro ao registrar o usuário:', error);
+          },
+        });
+    } else {
+      console.error('Preencha todos os campos antes de enviar o formulário.');
+    }
+  }
+
+  areAllFieldsFilled(): boolean {
+    return (
+      this.newUser.nome !== '' &&
+      this.newUser.telefone !== '' &&
+      this.newUser.email !== '' &&
+      this.newUser.cpf !== '' &&
+      this.newUser.cep !== '' &&
+      this.newUser.uf !== '' &&
+      this.newUser.cidade !== '' &&
+      this.newUser.bairro !== '' &&
+      this.newUser.rua !== ''
+    );
+  }
 }
