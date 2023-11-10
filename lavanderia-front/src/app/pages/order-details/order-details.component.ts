9d27297bc5a91183f502b9bd3b7b7cb280ddddca
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-10 00:37:24 -0300

    :sparkles: feat: consumo do get por id e html dinamico

--- lavanderia-front/src/app/pages/order-details/order-details.component.ts
+++ lavanderia-front/src/app/pages/order-details/order-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { IStatusOrder } from '../order/order.component';
+import { environment } from 'src/app/environment';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-order-details',
@@ -6,6 +10,29 @@ import { Component } from '@angular/core';
 })
 export class OrderDetailsComponent {
   isConfirmationModalOpen: boolean = false;
+  apiUrl = environment.apiUrl;
+  pedidos: IStatusOrder[] = [];
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    const orderId = this.route.snapshot.paramMap.get('id');
+  
+    this.http.get<IStatusOrder>(this.apiUrl + 'order/' + orderId).subscribe({
+      next: (data: IStatusOrder) => {
+        // Convert items from object to array
+        data.items = Object.values(data.items);
+  
+        this.pedidos = [data]; // Wrap data in an array
+        console.log(this.pedidos);
+      },
+      error: (error: any) => {
+        console.error('Erro ao buscar os dados:', error);
+      },
+    });
+  }
+  
+
 
   openConfirmationModal(): void {
     this.isConfirmationModalOpen = true;
