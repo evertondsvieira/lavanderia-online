7bf10ae8902a22ee33491f4320f492c143ce703c
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-11 19:32:02 -0300

    :sparkles: feat: removendo mock e consumindo da api no custom report

--- lavanderia-front/src/app/pages/report-customer-details/report-customer-details.component.ts
+++ lavanderia-front/src/app/pages/report-customer-details/report-customer-details.component.ts
@@ -2,6 +2,8 @@ import * as html2pdf from 'html2pdf.js';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { IReport } from '../report-customer/report-customer.component';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/app/environment';
 
 @Component({
   selector: 'app-report-customer-details',
@@ -11,169 +13,26 @@ export class ReportCustomerDetailsComponent implements OnInit {
   @ViewChild('pdfContent') pdfContent!: ElementRef;
   pdfDataUri: string | null = null;
   pdfConvertido = false;
-
+  apiUrl = environment.apiUrl;
   selectedReport: IReport | null = null;
+  reports: IReport[] = []
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.route.params.subscribe((params) => {
       const customerId = +params['id'];
-      this.selectedReport = this.reports.find(
-        (report) => report.id === customerId,
-      )!;
+  
+      this.http.get<IReport>(`${this.apiUrl}user/${customerId}`).subscribe({
+        next: (data: IReport) => {
+          this.selectedReport = data;
+        },
+        error: (error: any) => {
+          console.error('Erro ao buscar os dados:', error);
+        },
+      });
     });
   }
-
-  reports: IReport[] = [
-    {
-      id: 1,
-      name: 'João da Silva',
-      email: 'joao@example.com',
-      cpf: '123.456.789-00',
-      phone: 123456789,
-      cep: 12345678,
-      uf: 'SP',
-      city: 'São Paulo',
-      neighborhood: 'Centro',
-      street: 'Avenida Paulista',
-      order: [
-        {
-          id: 1,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 2,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 3,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 4,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Maria Oliveira',
-      email: 'maria@example.com',
-      cpf: '987.654.321-00',
-      phone: 123456789,
-      cep: 54321876,
-      uf: 'RJ',
-      city: 'Rio de Janeiro',
-      neighborhood: 'Copacabana',
-      street: 'Rua Nossa Senhora de Copacabana',
-      order: [
-        {
-          id: 1,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 2,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 3,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: 'Carlos Souza',
-      email: 'carlos@example.com',
-      cpf: '111.222.333-44',
-      phone: 123456789,
-      cep: 98765432,
-      uf: 'MG',
-      city: 'Belo Horizonte',
-      neighborhood: 'Savassi',
-      street: 'Avenida Getúlio Vargas',
-      order: [
-        {
-          id: 1,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 100,
-          status: 'Finalizado',
-        },
-        {
-          id: 2,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 100,
-          status: 'Finalizado',
-        },
-      ],
-    },
-    {
-      id: 4,
-      name: 'Pedro Silva',
-      email: 'pedro@example.com',
-      cpf: '555.666.777-88',
-      phone: 987654321,
-      cep: 12345678,
-      uf: 'SP',
-      city: 'São Paulo',
-      neighborhood: 'Vila Madalena',
-      street: 'Rua Augusta',
-      order: [
-        {
-          id: 1,
-          date: '25/09/2023',
-          deliveryDate: '25/09/2023',
-          value: 75,
-          status: 'Pendente',
-        },
-      ],
-    },
-    {
-      id: 5,
-      name: 'Ana Santos',
-      email: 'ana@example.com',
-      cpf: '777.888.999-11',
-      phone: 999999999,
-      cep: 54321098,
-      uf: 'PE',
-      city: 'Recife',
-      neighborhood: 'Boa Viagem',
-      street: 'Rua da Aurora',
-      order: [
-        {
-          id: 1,
-          date: '27/09/2023',
-          deliveryDate: '27/09/2023',
-          value: 120,
-          status: 'Entregue',
-        },
-      ],
-    },
-  ];
 
   convertToPDF(report: IReport) {
     this.selectedReport = report;
@@ -181,7 +40,7 @@ export class ReportCustomerDetailsComponent implements OnInit {
     const content = this.pdfContent.nativeElement;
     const options = {
       margin: 10,
-      filename: `${report.name}.pdf`,
+      filename: `${report.nome}.pdf`,
       image: { type: 'jpeg', quality: 0.98 },
       html2canvas: { scale: 2 },
       jsPDF: { orientation: 'landscape' },
--- lavanderia-front/src/app/pages/report-customer/report-customer.component.ts
+++ lavanderia-front/src/app/pages/report-customer/report-customer.component.ts
@@ -1,22 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/app/environment';
 
 export interface IReport {
   id: number;
-  name: string;
+  nome: string;
   email: string;
   cpf: string;
-  phone: number;
+  telefone: number;
   cep: number;
   uf: string;
-  city: string;
-  neighborhood: string;
-  street: string;
+  cidade: string;
+  bairro: string;
+  rua: string;
   order: {
     id: number;
-    date: string;
-    deliveryDate: string;
-    value: number;
+    data: string;
+    prazo: string;
+    valor: number;
     status: string;
   }[];
 }
@@ -26,157 +28,21 @@ export interface IReport {
   templateUrl: './report-customer.component.html',
 })
 export class ReportCustomerComponent {
-  reports: IReport[] = [
-    {
-      id: 1,
-      name: 'João da Silva',
-      email: 'joao@example.com',
-      cpf: '123.456.789-00',
-      phone: 123456789,
-      cep: 12345678,
-      uf: 'SP',
-      city: 'São Paulo',
-      neighborhood: 'Centro',
-      street: 'Avenida Paulista',
-      order: [
-        {
-          id: 1,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 2,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 3,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 4,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Maria Oliveira',
-      email: 'maria@example.com',
-      cpf: '987.654.321-00',
-      phone: 123456789,
-      cep: 54321876,
-      uf: 'RJ',
-      city: 'Rio de Janeiro',
-      neighborhood: 'Copacabana',
-      street: 'Rua Nossa Senhora de Copacabana',
-      order: [
-        {
-          id: 1,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 2,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-        {
-          id: 3,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 30,
-          status: 'Finalizado',
-        },
-      ],
-    },
-    {
-      id: 3,
-      name: 'Carlos Souza',
-      email: 'carlos@example.com',
-      cpf: '111.222.333-44',
-      phone: 123456789,
-      cep: 98765432,
-      uf: 'MG',
-      city: 'Belo Horizonte',
-      neighborhood: 'Savassi',
-      street: 'Avenida Getúlio Vargas',
-      order: [
-        {
-          id: 1,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 100,
-          status: 'Finalizado',
-        },
-        {
-          id: 2,
-          date: '24/09/2023',
-          deliveryDate: '24/09/2023',
-          value: 100,
-          status: 'Finalizado',
-        },
-      ],
-    },
-    {
-      id: 4,
-      name: 'Pedro Silva',
-      email: 'pedro@example.com',
-      cpf: '555.666.777-88',
-      phone: 987654321,
-      cep: 12345678,
-      uf: 'SP',
-      city: 'São Paulo',
-      neighborhood: 'Vila Madalena',
-      street: 'Rua Augusta',
-      order: [
-        {
-          id: 1,
-          date: '25/09/2023',
-          deliveryDate: '25/09/2023',
-          value: 75,
-          status: 'Pendente',
-        },
-      ],
-    },
-    {
-      id: 5,
-      name: 'Ana Santos',
-      email: 'ana@example.com',
-      cpf: '777.888.999-11',
-      phone: 999999999,
-      cep: 54321098,
-      uf: 'PE',
-      city: 'Recife',
-      neighborhood: 'Boa Viagem',
-      street: 'Rua da Aurora',
-      order: [
-        {
-          id: 1,
-          date: '27/09/2023',
-          deliveryDate: '27/09/2023',
-          value: 120,
-          status: 'Entregue',
-        },
-      ],
-    },
-  ];
+  reports: IReport[] = []
+  apiUrl = environment.apiUrl;
 
-  constructor(private router: Router) {}
+  constructor(private http: HttpClient, private router: Router) {}
+
+  ngOnInit(): void {
+    this.http.get<IReport[]>(this.apiUrl + 'user').subscribe({
+      next: (data: IReport[]) => {
+        this.reports = data
+      },
+      error: (error: any) => {
+        console.error('Erro ao buscar os dados:', error)
+      },
+    })
+  }
 
   showDetails(report: any) {
     const customerId = report.id;
