98f72113f2059faeee6200dfec10b1309cc3225d
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-11 17:16:49 -0300

    :sparkles: feat: pegando informações da api e correção da ui

--- lavanderia-front/src/app/pages/user-home/user-home.component.ts
+++ lavanderia-front/src/app/pages/user-home/user-home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/app/environment';
+import { IStatusOrder } from '../order/order.component';
 
 @Component({
   selector: 'app-user-home',
   templateUrl: './user-home.component.html',
-  styleUrls: ['./user-home.component.css']
 })
-export class UserHomeComponent {
+export class UserHomeComponent implements OnInit {
+  apiUrl = environment.apiUrl;
+  pedidosAbertos: IStatusOrder[] = [];
+  isConfirmationModalOpen: boolean = false
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<IStatusOrder[]>(this.apiUrl + 'order').subscribe({
+      next: (pedidos: IStatusOrder[]) => {
+        this.pedidosAbertos = pedidos.filter(
+          (pedido) => pedido.status === 'EM ABERTO',
+        );
+      },
+      error: (error: any) => {
+        console.log('Erro ao buscar os dados', error);
+      },
+    });
+  }
+
+  onOpenModal(): void {
+    this.isConfirmationModalOpen = true;
+  }
+  
+  onCancelClick(): void {
+    this.isConfirmationModalOpen = false;
+  }
+
+  onCancelOrder(): void {
+    this.isConfirmationModalOpen = false;
+    console.log('Pedido cancelado');
+  }
 }
