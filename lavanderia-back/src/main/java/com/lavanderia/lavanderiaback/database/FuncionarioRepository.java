d5106e50db742aa6db53b09c4b9b26332cb598c1
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-12 01:22:55 -0300

    :sparkles: feat: adicionado findByEmail

--- lavanderia-back/src/main/java/com/lavanderia/lavanderiaback/database/FuncionarioRepository.java
+++ lavanderia-back/src/main/java/com/lavanderia/lavanderiaback/database/FuncionarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.lavanderia.lavanderiaback.entities.Funcionario;
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
-    
+  Funcionario findByEmail(String email);   
 }
