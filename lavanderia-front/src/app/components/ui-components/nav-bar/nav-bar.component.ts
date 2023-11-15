cf8767bfc16737a64332cabfc6db50a8e5c6e5f9
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-15 13:05:10 -0300

    :sparkles: feat: nav bar dinamica de acordo com o usuario

--- lavanderia-front/src/app/components/ui-components/nav-bar/nav-bar.component.ts
+++ lavanderia-front/src/app/components/ui-components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import { AuthService } from 'src/app/utils/AuthService';
 
 interface INaveItems {
   nome: string
   icone: string
   link: string
-  employee: boolean
+  role: string
 }
 
 @Component({
@@ -16,40 +17,50 @@ export class NavBarComponent {
   drawerNavigation!: ElementRef;
 
   navItems: INaveItems[] = [
-    { nome: 'Home', icone: 'home', link: 'user/home', employee: false },
-    { nome: 'Pedidos', icone: 'article', link: '/order', employee: false },
-    { nome: 'Serviços', icone: 'local_laundry_service', link: '/services', employee: false },
-    { nome: 'Carrinho', icone: 'shopping_cart', link: '/cart', employee: false },
-    { nome: 'Home', icone: 'home', link: 'employee/home', employee: true },
-    { nome: 'Cadastro', icone: 'assignment_ind', link: 'employee/maintenance', employee: true },
-    { nome: 'Crud', icone: 'create', link: 'employee/crud', employee: true },
-    { nome: 'Receita', icone: 'attach_money', link: 'report/income', employee: true },
-    { nome: 'Clientes', icone: 'people', link: 'report/customer', employee: true },
-    { nome: 'Loyal', icone: 'loyalty', link: 'report/loyalcustomer', employee: true },
+    { nome: 'Home', icone: 'home', link: 'user/home', role: 'usuario' },
+    { nome: 'Pedidos', icone: 'article', link: '/order', role: 'usuario' },
+    { nome: 'Serviços', icone: 'local_laundry_service', link: '/services', role: 'usuario' },
+    { nome: 'Carrinho', icone: 'shopping_cart', link: '/cart', role: 'usuario' },
+    { nome: 'Home', icone: 'home', link: 'employee/home', role: 'funcionario' },
+    { nome: 'Cadastro', icone: 'assignment_ind', link: 'employee/maintenance', role: 'funcionario' },
+    { nome: 'Crud', icone: 'create', link: 'employee/crud', role: 'funcionario' },
+    { nome: 'Receita', icone: 'attach_money', link: 'report/income', role: 'funcionario' },
+    { nome: 'Clientes', icone: 'people', link: 'report/customer', role: 'funcionario' },
+    { nome: 'Loyal', icone: 'loyalty', link: 'report/loyalcustomer', role: 'funcionario' },
   ]
 
+  constructor(private authService: AuthService) {}
+
+  getFilteredNavItems(): INaveItems[] {
+    const userRole = this.authService.getCurrentUser()?.role
+    if (userRole) {
+      return this.navItems.filter(item => item.role === userRole)
+    }
+    return [];
+  }
+
   toggleDrawer(): void {
-    const drawer = this.drawerNavigation.nativeElement;
-    drawer.classList.toggle('translate-x-0');
-    drawer.classList.toggle('-translate-x-full');
+    const drawer = this.drawerNavigation.nativeElement
+    drawer.classList.toggle('translate-x-0')
+    drawer.classList.toggle('-translate-x-full')
   }
 
   closeDrawer(): void {
-    const drawer = this.drawerNavigation.nativeElement;
-    drawer.classList.remove('translate-x-0');
-    drawer.classList.add('-translate-x-full');
+    const drawer = this.drawerNavigation.nativeElement
+    drawer.classList.remove('translate-x-0')
+    drawer.classList.add('-translate-x-full')
   }
 
-  isLanguagePopoverVisible = false;
-  isThemePopoverVisible = false;
+  isLanguagePopoverVisible = false
+  isThemePopoverVisible = false
 
   toggleLanguagePopover() {
-    this.isLanguagePopoverVisible = !this.isLanguagePopoverVisible;
+    this.isLanguagePopoverVisible = !this.isLanguagePopoverVisible
   }
 
   toggleThemePopover() {
-    this.isThemePopoverVisible = !this.isThemePopoverVisible;
+    this.isThemePopoverVisible = !this.isThemePopoverVisible
   }
 
-  quantidadeItensNoCarrinho: number = 0;
+  quantidadeItensNoCarrinho: number = 0
 }
