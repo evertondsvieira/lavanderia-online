b5bb32f4e74f5b570ff63ed7180d538d4567f4fd
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-15 22:35:45 -0300

    :sparkles: feat: put para mudar o status para cancelado

--- lavanderia-front/src/app/pages/user-home/user-home.component.ts
+++ lavanderia-front/src/app/pages/user-home/user-home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/app/environment';
 import { IStatusOrder } from '../order/order.component';
+import { AuthService } from 'src/app/utils/AuthService';
 
 @Component({
   selector: 'app-user-home',
@@ -11,11 +12,14 @@ export class UserHomeComponent implements OnInit {
   apiUrl = environment.apiUrl;
   pedidosAbertos: IStatusOrder[] = [];
   isConfirmationModalOpen: boolean = false
+  orderIdToCancel: number | null = null;
 
-  constructor(private http: HttpClient) {}
-
+  constructor(private http: HttpClient, private authService: AuthService) {}
+  
   ngOnInit(): void {
-    this.http.get<IStatusOrder[]>(this.apiUrl + 'order').subscribe({
+    const userId = this.authService.getUserId();
+
+    this.http.get<IStatusOrder[]>(this.apiUrl + `order/user/${userId}`).subscribe({
       next: (pedidos: IStatusOrder[]) => {
         this.pedidosAbertos = pedidos.filter(
           (pedido) => pedido.status === 'EM ABERTO',
@@ -27,16 +31,31 @@ export class UserHomeComponent implements OnInit {
     });
   }
 
-  onOpenModal(): void {
-    this.isConfirmationModalOpen = true;
+  onCancelOrder(): void {
+    if (this.orderIdToCancel !== null) {
+      const userId = this.authService.getUserId();
+  
+      this.http.put(this.apiUrl + `order/${this.orderIdToCancel}/user/${userId}`, { status: 'CANCELADO' })
+        .subscribe({
+          next: () => {
+            window.location.reload();
+          },
+          error: (error: any) => {
+            console.log('Erro ao cancelar o pedido', error);
+          },
+        });
+  
+      this.isConfirmationModalOpen = false;
+      this.orderIdToCancel = null;
+    }
   }
   
-  onCancelClick(): void {
-    this.isConfirmationModalOpen = false;
+  onOpenModal(id: number): void {
+    this.orderIdToCancel = id;
+    this.isConfirmationModalOpen = true;
   }
 
-  onCancelOrder(): void {
+  onCancelClick(): void {
     this.isConfirmationModalOpen = false;
-    console.log('Pedido cancelado');
   }
 }
