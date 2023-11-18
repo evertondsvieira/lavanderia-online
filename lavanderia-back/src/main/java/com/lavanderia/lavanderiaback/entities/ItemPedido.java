c71da514cd2ed619495e16dad0561db78f37de2e
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-18 20:40:39 -0300

    :sparkles: feat: criado entidade item pedido

---
+++ lavanderia-back/src/main/java/com/lavanderia/lavanderiaback/entities/ItemPedido.java
@@ -0,0 +1,57 @@
+package com.lavanderia.lavanderiaback.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+@Entity
+public class ItemPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonBackReference
+    @ManyToOne
+    private Pedido pedido;
+
+    @ManyToOne
+    private Item item;
+
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+}
