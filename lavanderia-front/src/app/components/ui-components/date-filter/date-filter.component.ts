0dfad39f097fbd9d9d1e134dd964f3ade67c94be
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-11-28 23:54:01 -0300

    :sparkles: feat: criado chamadas de filtro

--- lavanderia-front/src/app/components/ui-components/date-filter/date-filter.component.ts
+++ lavanderia-front/src/app/components/ui-components/date-filter/date-filter.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, Output, EventEmitter } from '@angular/core';
+import { ReportIncomeComponent } from 'src/app/pages/report-income/report-income.component';
 
 @Component({
   selector: 'app-date-filter',
@@ -6,6 +7,10 @@ import { Component } from '@angular/core';
 })
 export class DateFilterComponent {
   showCustomDateRange: boolean = false;
+  @Input() startDate: string | null = null;
+  @Input() endDate: string | null = null;
+  @Input() reportIncomeComponent: ReportIncomeComponent | undefined;
+  @Output() filterApplied = new EventEmitter<{ startDate: string | null; endDate: string | null; }>();
 
   openModal() {
     const modal = document.getElementById('modal');
@@ -19,5 +24,39 @@ export class DateFilterComponent {
 
   toggleCustom() {
     this.showCustomDateRange = !this.showCustomDateRange;
+  }
+
+  applyFilter() {  
+    this.filterApplied.emit({ startDate: this.startDate, endDate: this.endDate });
+  
+    if (this.reportIncomeComponent) {  
+      this.reportIncomeComponent.applyFilter();
+    }
+
+    this.closeModal()
+  }
+
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+  
+  filterToday() {
+    const today = new Date().toISOString().split('T')[0];
+    this.startDate = today;
+    this.endDate = today;
+    this.applyFilter();
+  }
+
+  filterAll() {
+    const today = new Date();
+    const firstDayOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+    const lastDayOfMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+  
+    this.startDate = this.formatDate(firstDayOfMonth);
+    this.endDate = this.formatDate(lastDayOfMonth);
+    this.applyFilter();
   }
 }
