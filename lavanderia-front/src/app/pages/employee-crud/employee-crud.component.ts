aa0c337a184023cd31873ab3e08bd8f59225ecdc
Everton Vieira <96546899+evertondsvieira@users.noreply.github.com>
2023-12-01 01:25:26 -0300

    :sparkles: feat: validação de formulário no crud

--- lavanderia-front/src/app/pages/employee-crud/employee-crud.component.ts
+++ lavanderia-front/src/app/pages/employee-crud/employee-crud.component.ts
@@ -16,10 +16,10 @@ export interface ClothingItem {
   templateUrl: './employee-crud.component.html',
 })
 export class EmployeeCrudComponent implements OnInit {
-  apiUrl = environment.apiUrl
-  listaVazia: boolean = false
-  clothingItems: ClothingItem[] = []
-  itemEmEdicao: ClothingItem | null = null
+  apiUrl = environment.apiUrl;
+  listaVazia: boolean = false;
+  clothingItems: ClothingItem[] = [];
+  itemEmEdicao: ClothingItem | null = null;
   RemovingItem: ClothingItem | null = null;
   showErrorAlert: boolean = false;
 
@@ -33,10 +33,12 @@ export class EmployeeCrudComponent implements OnInit {
     prazo: '',
     descricao: '',
   };
- 
+
+  errorMessages = { nome: '', imgUrl: '', valor: '', prazo: '', descricao: '' };
+
   editItem(item: ClothingItem) {
     this.itemEmEdicao = { ...item };
-    this.novoItem = {... item};
+    this.novoItem = { ...item };
 
     if (!this.itemEmEdicao) {
       this.itemEmEdicao = {
@@ -60,16 +62,16 @@ export class EmployeeCrudComponent implements OnInit {
         this.showErrorAlert = true;
 
         setTimeout(() => {
-        this.showErrorAlert = false;
-         }, 5000);
+          this.showErrorAlert = false;
+        }, 5000);
       },
     });
   }
 
   addItem() {
-    if (this.novoItem.nome && this.novoItem.valor && this.novoItem.prazo) {
+    if (this.novoItem.nome && this.novoItem.valor && this.novoItem.prazo && this.validateForm()) {
       const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-  
+
       this.http
         .post<ClothingItem>(this.apiUrl + 'item', this.novoItem, { headers })
         .subscribe({
@@ -89,27 +91,26 @@ export class EmployeeCrudComponent implements OnInit {
             console.error('Erro ao adicionar o item:', error);
             this.showErrorAlert = true;
 
-        setTimeout(() => {
-        this.showErrorAlert = false;
-         }, 5000);
+            setTimeout(() => {
+              this.showErrorAlert = false;
+            }, 5000);
           },
         });
     }
   }
- 
 
   putItem() {
-    if (this.itemEmEdicao) {
+    if (this.itemEmEdicao && this.validateForm()) {
       this.itemEmEdicao = { ...this.itemEmEdicao, ...this.novoItem };
-  
+
       const index = this.clothingItems.findIndex(
-        (item) => item.id === this.itemEmEdicao?.id
+        (item) => item.id === this.itemEmEdicao?.id,
       );
       if (index !== -1) {
         this.http
           .put<ClothingItem>(
             `${this.apiUrl}item/${this.itemEmEdicao.id}`,
-            this.itemEmEdicao
+            this.itemEmEdicao,
           )
           .subscribe({
             next: (data: ClothingItem) => {
@@ -120,23 +121,25 @@ export class EmployeeCrudComponent implements OnInit {
               console.error('Erro ao atualizar o item:', error);
               this.showErrorAlert = true;
 
-        setTimeout(() => {
-        this.showErrorAlert = false;
-         }, 5000);
+              setTimeout(() => {
+                this.showErrorAlert = false;
+              }, 5000);
             },
           });
       }
     }
   }
-  
+
   updateItem() {
-    if (this.novoItem.id === 0) {
-      this.addItem()
-    } else {
-      this.putItem()
-    }   
-  }
+    if (!this.novoItem.nome && !this.novoItem.valor && !this.novoItem.prazo && !this.novoItem.imgUrl) return;
   
+    if (this.itemEmEdicao) {
+      this.putItem();
+    } else {
+      this.addItem();
+    }
+  }
+
   removeItem(item: ClothingItem) {
     const index = this.clothingItems.indexOf(item);
     if (index !== -1) {
@@ -151,9 +154,9 @@ export class EmployeeCrudComponent implements OnInit {
           console.error('Erro ao excluir o item:', error);
           this.showErrorAlert = true;
 
-        setTimeout(() => {
-        this.showErrorAlert = false;
-         }, 5000);
+          setTimeout(() => {
+            this.showErrorAlert = false;
+          }, 5000);
         },
       });
     }
@@ -170,13 +173,87 @@ export class EmployeeCrudComponent implements OnInit {
       descricao: '',
     };
   }
+
+  validateForm(): boolean {
+    this.errorMessages = {
+      nome: '',
+      imgUrl: '',
+      valor: '',
+      prazo: '',
+      descricao: '',
+    };
+  
+    this.validateName();
+    this.validateImgUrl();
+    this.validatePrazo();
+    this.validateValor();
+    this.validateDescricao();
+  
+    return !Object.values(this.errorMessages).some(message => message !== '');
+  }
+  
+
+  validateField(fieldName: string): void {
+    switch (fieldName) {
+      case 'nome':
+        this.validateName();
+        break;
+      case 'imgUrl':
+        this.validateImgUrl();
+        break;
+      case 'prazo':
+        this.validatePrazo();
+        break;
+      case 'valor':
+        this.validateValor();
+        break;
+      case 'descricao':
+        this.validateDescricao();
+        break;
+    }
+  }
+
+  validateName(): void {
+    this.errorMessages.nome = '';
+
+    if (!this.novoItem.nome) {
+      this.errorMessages.nome = 'Nome é obrigatório';
+    }
+  }
+
+  validateImgUrl(): void {
+    this.errorMessages.imgUrl = '';
+
+    if (!this.novoItem.imgUrl) {
+      this.errorMessages.imgUrl = 'Imagem é obrigatória';
+    }
+  }
+
+  validatePrazo(): void {
+    this.errorMessages.prazo = '';
+
+    if (!this.novoItem.prazo) {
+      this.errorMessages.prazo = 'Prazo é obrigatório';
+    }
+  }
+
+  validateValor(): void {
+    this.errorMessages.valor = '';
+
+    if (!this.novoItem.valor) {
+      this.errorMessages.valor = 'Valor é obrigatório';
+    }
+  }
+
+  validateDescricao(): void {}
+
   openConfirmationModal(clothingItems: ClothingItem) {
     this.RemovingItem = clothingItems;
   }
 
   confirmRemoveItem(clothingItems: ClothingItem) {
     if (clothingItems) {
-      this.removeItem(clothingItems)
+      this.removeItem(clothingItems);
       this.RemovingItem = null;
     }
   }
